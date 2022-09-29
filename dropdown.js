a08005c662f7b018f4f3e784b24219f4feb50b2a
Vedant Ghonge <vedantghonge0@gmail.com>
2022-09-29 23:58:21 +0530

    completed v1

---
+++ dropdown.js
@@ -0,0 +1,4 @@
+$(".lessons-dropdown-icon").click(function(){
+    $(".lessons-content").toggleClass("lessons-content-inactive");
+    $(".lessons-dropdown-icon").toggleClass("lessons-dropdown-icon-active");
+});
